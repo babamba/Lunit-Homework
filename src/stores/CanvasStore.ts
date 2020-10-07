7b40791fb36948c3e3a723d6d5559bf31701a8b2
jw.kim <babamba88@gintlab.com>
2020-10-07 18:45:41 +0900

    refactor: :sparkles: store Action 기능 추가

--- src/stores/CanvasStore.ts
+++ src/stores/CanvasStore.ts
@@ -1,5 +1,15 @@
-import { observable, action, computed } from 'mobx';
+import { observable, action } from 'mobx';
 import RootStoreModel from './index';
+
+interface Polygon {
+  key: number;
+  lines: Coordinate[];
+}
+
+type Coordinate = {
+  x: number;
+  y: number;
+};
 
 class CanvasStore {
   rootStore: RootStoreModel;
@@ -7,11 +17,51 @@ class CanvasStore {
     this.rootStore = rootStore;
   }
 
-  @observable drawItems = [];
+  @observable drawItems: Polygon[] = [];
+  @observable selectItems: number[] = [];
 
   @action.bound
-  async test() {
-    console.log('test : ', this.drawItems);
+  async initStorage() {
+    console.log('----> [Store] initStorage');
+    const local = await localStorage.getItem('polygonList');
+    if (local) this.drawItems = JSON.parse(local);
+  }
+
+  @action.bound
+  async addPolygon(param: Polygon) {
+    const local = await localStorage.getItem('polygonList');
+    if (local) {
+      const list = JSON.parse(local) as Polygon[];
+      const newList = [...list, param];
+      await localStorage.setItem('polygonList', JSON.stringify(newList));
+      this.setDrawItems(newList);
+    } else {
+      await localStorage.setItem('polygonList', JSON.stringify([param]));
+      this.setDrawItems([param]);
+    }
+  }
+
+  @action.bound
+  async deletePolygon(list: number[]) {
+    const local = await localStorage.getItem('polygonList');
+    if (local) {
+      const filterArr: Polygon[] = JSON.parse(local).filter(
+        (item: Polygon, idx: number) => list.indexOf(item.key) < 0
+      );
+      await localStorage.setItem('polygonList', JSON.stringify(filterArr));
+      this.setDrawItems(filterArr);
+      this.setSelectedItem([]);
+    }
+  }
+
+  @action.bound
+  setDrawItems(newList: Polygon[]) {
+    this.drawItems = newList;
+  }
+
+  @action.bound
+  setSelectedItem(list: number[]) {
+    this.selectItems = list;
   }
 }
 
