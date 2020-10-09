730888108b3f01ae0b1888ee375baf78e2c8a4aa
babamba <orochi13@naver.com>
2020-10-09 13:47:11 +0900

    refactor: :sparkles: 기존 공통사용 interface 및 type export

---
+++ src/interface/polygon.ts
@@ -0,0 +1,11 @@
+export interface Polygon {
+    key: number;
+    lines: Coordinate[];
+    moves: Coordinate[];
+    isMerged: boolean;
+  }
+  
+export type Coordinate = {
+    x: number;
+    y: number;
+};
