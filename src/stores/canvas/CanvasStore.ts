5532b9de8cf25dc9b27bd19883e6a96534875b50
babamba <orochi13@naver.com>
2020-10-09 13:45:46 +0900

    refactor: :recycle: store 폴더구조 수정 및 불필요 파일 삭제

--- src/stores/canvas/CanvasStore.ts
+++ src/stores/canvas/CanvasStore.ts
@@ -1,17 +1,6 @@
+import RootStoreModel from 'store/index';
 import { observable, action } from 'mobx';
-import RootStoreModel from './index';
-
-interface Polygon {
-  key: number;
-  lines: Coordinate[];
-  moves: Coordinate[];
-  isMerged: boolean;
-}
-
-type Coordinate = {
-  x: number;
-  y: number;
-};
+import { Polygon } from 'interface/polygon'
 
 class CanvasStore {
   rootStore: RootStoreModel;
@@ -26,7 +15,7 @@ class CanvasStore {
   async initStorage() {
     console.log('----> [Store] initStorage');
     const local = await localStorage.getItem('polygonList');
-    if (local) this.drawItems = JSON.parse(local);
+    if (local) this.setDrawItems(JSON.parse(local) as Polygon[]);
   }
 
   @action.bound
@@ -37,7 +26,7 @@ class CanvasStore {
       const newList = [...list, param];
       await localStorage.setItem('polygonList', JSON.stringify(newList));
       this.setDrawItems(newList);
-    } else {
+    } else { 
       await localStorage.setItem('polygonList', JSON.stringify([param]));
       this.setDrawItems([param]);
     }
@@ -53,6 +42,18 @@ class CanvasStore {
       await localStorage.setItem('polygonList', JSON.stringify(filterArr));
       this.setDrawItems(filterArr);
       this.setSelectedItem([]);
+    }
+  }
+
+  @action.bound
+  getMaxIndex(){
+    // 아이템을 추가할때 마다, 리스트에 key의 최대값을 찾아 ++값으로 리턴.
+    console.log('this.drawItems : ', this.drawItems)
+    if (this.drawItems.length > 0) {
+      const max = Math.max(...this.drawItems.map(o => o.key));
+      return max + 1;
+    } else {
+      return 0;
     }
   }
 
